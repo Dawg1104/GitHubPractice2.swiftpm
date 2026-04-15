821d83aa4f6ac3ac786175e30fb6da7f3c5c6c9f
Kian <ktrotter8454@stu.d214.org>
2026-04-15 10:26:55 -0500

    added Textfield number1, with some modifiers

--- ContentView.swift
+++ ContentView.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 struct ContentView: View {
     @State var number1 = 0
-    
     @State var number2: Int = 0
     var body: some View {
+        
+        TextField("enter a number", value: $number1, format: .number)
+            .textFieldStyle(.roundedBorder)
+        
         
     }
 }
