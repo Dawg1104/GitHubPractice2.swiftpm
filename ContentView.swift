85124fb0b2dc64d04d9a12bd7862707083974b6f
Kian <ktrotter8454@stu.d214.org>
2026-04-15 10:30:48 -0500

    added the calculate button

--- ContentView.swift
+++ ContentView.swift
@@ -8,7 +8,18 @@ struct ContentView: View {
         TextField("enter a number", value: $number1, format: .number)
             .textFieldStyle(.roundedBorder)
         
-        
         TextField("", value: $number2, format: .number)
+        
+        Button(action: answer = number1 + number2) {
+            ZStack{
+                RoundedRectangle(cornerRadius: 25)
+                    .frame(width: 300, height: 130)
+                    .foregroundStyle(.blue)
+                Text("Add")
+                    .foregroundStyle(.white)
+                    .bold()
+                    .font(.largeTitle)
+            }
+        }
     }
 }
